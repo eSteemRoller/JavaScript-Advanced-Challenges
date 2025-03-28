9622004c1e1b19375a2df5456c4f2faef02b2601
Jason <jasondoroughonfb@live.com>
2025-03-27 23:40:24 -0700

    Second commit

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 // Q1. Show Rating
 function showRating(userRating) {
   let convertedRating = ``;
-  for (let i = 1; i <= userRating; ++i) {
-    userRating = userRating + `*`;
+  for (let i = 1; i <= userRating; ++i) { // Math.floor?
+    convertedRating = convertedRating + `*`;
     if (i !== userRating) {
-      userRating = userRating + ` `;
+      convertedRating += ` `; // like above but short syntax
     }
+  }
+  if (!Number.isInteger(userRating)) { // Check if value is !NOT whole (is decimal)
+    convertedRating += `.`;
   }
   return convertedRating;
 }
-console.log(showRating(5));
+console.log(showRating(4.5));
 
+
+// Q2. Sort by Lowest to Highest Price
+function sortLowToHigh(arr1) {
+  return arr1.sort((a, b) => a - b);
+}
+console.log(sortLowToHigh([20, 40, 10, 30, 50, 10]));
+
+
+// Q3. 
