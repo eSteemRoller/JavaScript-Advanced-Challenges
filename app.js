f33199f75fd37c85f16fef5c03e9798d2c30d51f
Jason <jasondoroughonfb@live.com>
2025-03-29 03:36:35 -0700

    Third commit

--- app.js
+++ app.js
@@ -23,4 +23,47 @@ function sortLowToHigh(arr1) {
 console.log(sortLowToHigh([20, 40, 10, 30, 50, 10]));
 
 
-// Q3. 
+// Q3. Sort by Highest to Lowest Price
+function sortPriceHighToLow(objectPrices) {
+  return objectPrices.sort((a, b) => { // Syntax can be simplified ton 1 line w/o {}.
+    return b.price - a.price;
+  });
+}
+console.log(sortPriceHighToLow([
+    { id: 1, price: 50 },
+    { id: 2, price: 30 },
+    { id: 3, price: 60 },
+    { id: 4, price: 10 }
+  ])
+);
+
+
+// Q4. Promises (Watch Promises videos)
+// https://www.youtube.com/watch?v=vn3tm0quoqE
+
+
+// Q5. Find All the Posts by a Single User
+// API: https://jsonplaceholder.typicode.com/posts
+async function postsByUser(userID) {
+  const allUserPosts = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  // console.log(await allUserPosts.json());
+  const userPosts = await allUserPosts.json();
+  const filteredPosts = userPosts.filter(element => element.userId === userID);
+  console.log(filteredPosts);
+}
+postsByUser(4);
+
+
+// Q6. Return the First 6 Incomplete ToDo's
+// API: https://jsonplaceholder.typicode.com/todos
+async function returnIncompleteToDos(numberOfToDos) {
+  const allToDos = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  const ToDoStatuses = await allToDos.json();
+  let incompleteToDos = ToDoStatuses.filter(element => element.completed === false).slice(0, 6);
+  // for (let i = 0; i < numberOfToDos; ++i) {
+  // console.log(incompleteToDos);
+  // }
+  console.log(incompleteToDos);
+}
+returnIncompleteToDos(6);
+
